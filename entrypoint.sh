a4589d87f2791f71a597aa507459aa1b35baf506
Naoki Ainoya <ainonic@gmail.com>
2022-04-13 15:53:25 +0900

    workaround https://github.com/repo-sync/pull-request/issues/84

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,10 @@ fi
 
 DESTINATION_BRANCH="${INPUT_DESTINATION_BRANCH:-"master"}"
 
+# workaround
+# https://github.com/repo-sync/pull-request/issues/84
+git config --global --add safe.directory /github/workspace
+
 # Github actions no longer auto set the username and GITHUB_TOKEN
 git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@${GITHUB_SERVER_URL#https://}/$GITHUB_REPOSITORY"
 
